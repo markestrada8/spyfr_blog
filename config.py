e74dc815ddffbc5ed211cd9efbe34be0ead377b5
Mark Estrada <mark.estrada.dev>
2023-06-26 15:46:15 -0500

    reconfigure db connection config / reinstantiate database instances from flask shell with correct context provided for app instance from run.py

--- config.py
+++ config.py
@@ -7,18 +7,21 @@ class Config:
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///dev.db")
-    DEBUG=True
-    SQLALCHEMY_ECHO=True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI="sqlite:///dev.db"
-    DEBUG = config("DEBUG", cast=bool)
-    SQLALCHEMY_ECHO = config("ECHO", cast=bool)
-    SQLALCHEMY_TRACK_MODIFICATIONS = config("SQLALCHEMY_TRACK_MODIFICATIONS", cast=bool)
+    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
+    # DEBUG = config("DEBUG", cast=bool)
+    # SQLALCHEMY_ECHO = config("ECHO", cast=bool)
+    # SQLALCHEMY_TRACK_MODIFICATIONS = config("SQLALCHEMY_TRACK_MODIFICATIONS", cast=bool)
+    DEBUG = False
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):
     # ALSO NEED TO SET AUTH CREDENTIALS SETTINGS PROBABLY
     SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
-    SQLALCHEMY_ECHO=False
-    TESTING=True
+    SQLALCHEMY_ECHO = False
+    TESTING = True
